91791f6f9a993d526a085a1517cd3cf7057d1e94
Saikat Guha <sktguha@gmail.com>
2020-09-16 17:34:41 +0530

    Update inflationCalc.js

--- inflationCalc.js
+++ inflationCalc.js
@@ -2,7 +2,13 @@
 function calc(init=3.2, yr=[], marker=[]){
     return yr.reduce((acc, inf, i)=>{ const res= acc + (Number(inf)/100)*acc; marker[i] && console.log('in '+marker[i]+' it is '+acc); return res;},init)
 }
-calc(42, [3.6, 3.43, 4.54, 2.19], [2017,2018,2019,2020]);
+calc(42, [3.6, 3.43, 4.54, 2.19], [2017,2018,2019,2020]); //47.047664156639996
+//output:
+// in 2017 it is 42
+// in 2018 it is 43.512
+// in 2019 it is 45.0044616
+// in 2020 it is 47.047664156639996
+
 calc(8, [3.43,3.43,3.43,3.6, 3.43, 4.54, 2.19], [2014,2015,2016,2017,2018,2019,2020])
 calc(12, [3.43,3.43,3.6, 3.43, 4.54, 2.19], [2015,2016,2017,2018,2019,2020])
 //usage: calc accepts three arguments , intial salary, array of inflation per year, and third argument is the years(just for display purposes)
