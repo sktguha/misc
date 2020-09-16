b795d20a59afbef2f86178142fb72cc98e9e2892
Saikat Guha <sktguha@gmail.com>
2020-09-16 17:39:28 +0530

    Update inflationCalc.js

--- inflationCalc.js
+++ inflationCalc.js
@@ -11,7 +11,10 @@ calc(42, [3.6, 3.43, 4.54, 2.19], [2017,2018,2019,2020]); //47.047664156639996
 
 calc(8, [3.43,3.43,3.43,3.6, 3.43, 4.54, 2.19], [2014,2015,2016,2017,2018,2019,2020])
 calc(12, [3.43,3.43,3.6, 3.43, 4.54, 2.19], [2015,2016,2017,2018,2019,2020])
+calc(42, Array(4).fill(20), [2020,2021,2022,2023]);
 //usage: calc accepts three arguments , intial salary, array of inflation per year, and third argument is the years(just for display purposes)
+calc(42, Array(4).fill(20), [2020,2021,2022,2023]); // 72.576
+calc(42, Array(4).fill(10), [2020,2021,2022,2023]); // 55.902
 
 function revCalc(init,yr, marker=[]){ marker=marker.reverse(); return yr.reduce((acc, inf, i)=>{ const res= (acc/(1 + (inf/100))); marker[i] && console.log('in '+marker[i]+' it is '+acc); return res;},init) }
 revCalc(42, [3.6, 3.43, 4.54, 2.19], [2017,2018,2019,2020]);
