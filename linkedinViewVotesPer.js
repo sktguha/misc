12b0a1e9ee5c47101617e9ce322e6db85fcd6aee
Saikat Guha <sktguha@gmail.com>
2020-09-28 20:19:11 +0530

    improve perf

--- linkedinViewVotesPer.js
+++ linkedinViewVotesPer.js
@@ -11,22 +11,27 @@
 (function() {
     'use strict';
      window.onload= ()=>{
-        setInterval(()=>
-         document.querySelectorAll('.occludable-update').forEach(dv=>{
-            try{
-                let ns = dv.querySelector('.feed-shared-actor__name').firstElementChild;
-                if(ns.innerText.indexOf('%')!== -1) return;
-                let views = dv.getElementsByTagName('Strong')[0].innerText.split(' ')[0];
-                views = parseInt(views.split(",").join(""))
-                let votes = dv.querySelector('.social-details-social-counts__reactions-count');
-                let ve = votes;
-                votes = votes ? votes.innerText*1:0;
-                let per =  parseFloat((votes/views)*100).toFixed(3) + '%';
-                ns.innerText = ns.innerText.indexOf('%')=== -1 ? ns.innerText+' '+per: ns.innerText;
-            } catch(e){
-                console.log("error occurred",e);
-            }
-        }),1000)
+        var plen;
+        setInterval(()=>{
+            let divs = document.querySelectorAll('.occludable-update');
+            if(divs.length === plen) return;
+            plen = divs.length;
+            divs.forEach(dv=>{
+                try{
+                    let ns = dv.querySelector('.feed-shared-actor__name').firstElementChild;
+                    if(ns.innerText.indexOf('%')!== -1) return;
+                    let views = dv.getElementsByTagName('Strong')[0].innerText.split(' ')[0];
+                    views = parseInt(views.split(",").join(""))
+                    let votes = dv.querySelector('.social-details-social-counts__reactions-count');
+                    let ve = votes;
+                    votes = votes ? votes.innerText*1:0;
+                    let per =  parseFloat((votes/views)*100).toFixed(3) + '%';
+                    ns.innerText = ns.innerText.indexOf('%')=== -1 ? ns.innerText+' '+per: ns.innerText;
+                } catch(e){
+                    console.log("error occurred",e);
+                }
+            })
+        },1000)
      }
     // Your code here...
 })();
