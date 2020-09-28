ab847ca3c01dfb8be539fa4aa25337c37abb35cd
Saikat Guha <sktguha@gmail.com>
2020-09-28 20:12:59 +0530

    Update linkedinViewVotesPer.js

--- linkedinViewVotesPer.js
+++ linkedinViewVotesPer.js
@@ -13,15 +13,19 @@
      window.onload= ()=>{
         setInterval(()=>
          document.querySelectorAll('.occludable-update').forEach(dv=>{
-            let ns = dv.querySelector('.feed-shared-actor__name').firstElementChild;
-            if(ns.innerText.indexOf('%')!== -1) return;
-            let views = dv.getElementsByTagName('Strong')[0].innerText.split(' ')[0];
-            views = parseInt(views.split(",").join(""))
-            let votes = dv.querySelector('.social-details-social-counts__reactions-count');
-            let ve = votes;
-            votes = votes ? votes.innerText*1:0;
-            let per =  parseFloat((votes/views)*100).toFixed(3) + '%';
-            ns.innerText = ns.innerText.indexOf('%')=== -1 ? ns.innerText+' '+per: ns.innerText;
+            try{
+                let ns = dv.querySelector('.feed-shared-actor__name').firstElementChild;
+                if(ns.innerText.indexOf('%')!== -1) return;
+                let views = dv.getElementsByTagName('Strong')[0].innerText.split(' ')[0];
+                views = parseInt(views.split(",").join(""))
+                let votes = dv.querySelector('.social-details-social-counts__reactions-count');
+                let ve = votes;
+                votes = votes ? votes.innerText*1:0;
+                let per =  parseFloat((votes/views)*100).toFixed(3) + '%';
+                ns.innerText = ns.innerText.indexOf('%')=== -1 ? ns.innerText+' '+per: ns.innerText;
+            } catch(e){
+                console.log("error occurred",e);
+            }
         }),1000)
      }
     // Your code here...
