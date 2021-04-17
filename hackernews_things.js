ccadb13baafaf6c343507e132f6436da6819bd95
Saikat Guha <sktguha@gmail.com>
2021-04-18 00:30:20 +0530

    Create hackernews_things.js

---
+++ hackernews_things.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         HN comments hide
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://news.ycombinator.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     function doAcc(){
+        window.hf = !window.hf; [...document.querySelectorAll('.ind')].filter(el=> el.firstChild.width !== 0).forEach(td => td.parentElement.style.display =window.hf?"none":"block");
+     }
+     function att(fn){ try{fn()} catch(e){ console.error(e)}}
+     window.onload = ()=>doAcc();
+     window.onkeypress = ({key})=>{
+       if(key==="e") {
+           doAcc();
+       } else if(key==="f"){
+           att(()=>[...document.getElementsByTagName('a')].filter(a=>a.innerText==='favorite')[0].click());
+       }
+     }
+    // Your code here...
+})();
